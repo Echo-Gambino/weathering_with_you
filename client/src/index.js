f8cddc9323fba00797d86c92619f75848b743a31
Echo-Gambino <nkb012pomm@gmail.com>
2021-01-18 19:43:22 -0700

    initialized redux in the webapp

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,17 @@ import reportWebVitals from './reportWebVitals';
 import Header from "./Components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode store={store}>
     <Header />
     <App />
   </React.StrictMode>,
